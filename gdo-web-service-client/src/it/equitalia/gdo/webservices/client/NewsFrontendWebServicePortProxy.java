7da4a347071d7ce02a7e1c084d07daf061281cbc
Fabio V <f.vittori@solvingteam.it>
2022-11-24 09:52:50 +0100

    Revert "progetto gdo-web-service-client aggiornato alla versione che si trova nel jar di Geupom in eqs_sso"
    This reverts commit 13b76f621d390721ffe1c8b6959de17e753d237c.

--- gdo-web-service-client/src/it/equitalia/gdo/webservices/client/NewsFrontendWebServicePortProxy.java
+++ gdo-web-service-client/src/it/equitalia/gdo/webservices/client/NewsFrontendWebServicePortProxy.java
@@ -85,8 +85,4 @@ public class NewsFrontendWebServicePortProxy{
         return _getDescriptor().getProxy().recuperaNewsAttiveDestinateAdUtenteAgente(arg0);
     }
 
-    public ListaNews recuperaNewsAttivePerAltriUtenti(String arg0) throws BusinessException_Exception {
-        return _getDescriptor().getProxy().recuperaNewsAttivePerAltriUtenti(arg0);
-    }
-
 }

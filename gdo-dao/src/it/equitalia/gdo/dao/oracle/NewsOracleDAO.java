8dfa39f316a9c8ca4ffb8b081d3aeef95c3bfc66
Fabio V <f.vittori@solvingteam.it>
2022-11-30 10:46:15 +0100

    aggiunto param utente esterno nella query

--- gdo-dao/src/it/equitalia/gdo/dao/oracle/NewsOracleDAO.java
+++ gdo-dao/src/it/equitalia/gdo/dao/oracle/NewsOracleDAO.java
@@ -426,6 +426,9 @@ public class NewsOracleDAO extends EntitaVersionabileDAO<News> implements NewsDA
 		if(params.get(PARAM_ALTRI_UTENTI)!=null){
 			queryString+=" and n.altriUtenti = :" + PARAM_ALTRI_UTENTI + " ";
 		}
+		if(params.get(PARAM_UTENTE_ESTERNO)!=null){
+			queryString+=" and n.utenteEsterno = :" + PARAM_UTENTE_ESTERNO + " ";
+		}
 		
 		queryString+=" AND (n.dataInizioPubblicazione IS NULL OR current_date >= n.dataInizioPubblicazione )";
 				
@@ -443,6 +446,9 @@ public class NewsOracleDAO extends EntitaVersionabileDAO<News> implements NewsDA
 		}
 		if(params.get(PARAM_ALTRI_UTENTI)!=null){
 			q.setParameter(PARAM_ALTRI_UTENTI, params.get(PARAM_ALTRI_UTENTI));
+		}
+		if(params.get(PARAM_UTENTE_ESTERNO)!=null){
+			q.setParameter(PARAM_UTENTE_ESTERNO, params.get(PARAM_UTENTE_ESTERNO));
 		}
 		List<News> news= q.getResultList();
 		return news;
